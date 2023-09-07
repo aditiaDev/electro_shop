1ee9bb793ad61ae4ffcfb7c7265d6f3b52717c1f
unknown <urbanaditia@gmail.com>
2023-09-07 13:02:16 +0700

    Add midtrans

--- application/views/pages/front/checkout.php
+++ application/views/pages/front/checkout.php
@@ -147,6 +147,12 @@
       <!-- /row -->
     </div>
     <!-- /container -->
+
+    <form id="payment-form" method="post" action="<?=site_url()?>snap/finish">
+      <input type="hidden" name="result_type" id="result-type" value=""></div>
+      <input type="hidden" name="result_data" id="result-data" value=""></div>
+    </form>
+
   </div>
   <!-- /SECTION -->
 
@@ -159,6 +165,7 @@
   }
 ?>
 <script src="<?php echo base_url(); ?>assets/template/front/js/jquery.min.js"></script>
+<script type="text/javascript" src="https://app.sandbox.midtrans.com/snap/snap.js" data-client-key="SB-Mid-client-C_hVzhEuRXcHPsa6"></script>
 <script>
   // getKota()
 
@@ -338,31 +345,67 @@
 
   $("#btnCheckOut").click(function(){
     event.preventDefault();
-    // var $dataElements = $('#tb_item').find('td'),
-    // data = [];
 
-    // $.each($dataElements, function(i, elem){
-    //     data.push($(elem).html());
-    // });
-    formData = $("#FRM_DATA").serialize()
     $.ajax({
-      url: "<?php echo site_url('front/checkOutSave') ?>",
+      url: "<?php echo site_url('snap/token') ?>",
+      cache: false,
       type: "POST",
-      dataType: "JSON",
-      data: formData,
-      success: function(data){
-        // console.log(data)
-        if (data.status == "success") {
-          toastr.info(data.message)
-          setTimeout(() => {
-            window.location="<?php echo base_url('home');?>"
-          }, 500);
-          
-        }else{
-          toastr.error(data.message)
+      success: function(data) {
+        //location = data;
+
+        console.log('token = '+data);
+        
+        var resultType = document.getElementById('result-type');
+        var resultData = document.getElementById('result-data');
+
+        function changeResult(type,data){
+          $("#result-type").val(type);
+          $("#result-data").val(JSON.stringify(data));
+          //resultType.innerHTML = type;
+          //resultData.innerHTML = JSON.stringify(data);
         }
+
+        snap.pay(data, {
+          
+          onSuccess: function(result){
+            changeResult('success', result);
+            console.log(result.status_message);
+            console.log(result);
+            $("#payment-form").submit();
+          },
+          onPending: function(result){
+            changeResult('pending', result);
+            console.log(result.status_message);
+            $("#payment-form").submit();
+          },
+          onError: function(result){
+            changeResult('error', result);
+            console.log(result.status_message);
+            $("#payment-form").submit();
+          }
+        });
       }
-    })
+    });
+
+    // formData = $("#FRM_DATA").serialize()
+    // $.ajax({
+    //   url: "<?php echo site_url('front/checkOutSave') ?>",
+    //   type: "POST",
+    //   dataType: "JSON",
+    //   data: formData,
+    //   success: function(data){
+    //     // console.log(data)
+    //     if (data.status == "success") {
+    //       toastr.info(data.message)
+    //       setTimeout(() => {
+    //         window.location="<?php echo base_url('home');?>"
+    //       }, 500);
+          
+    //     }else{
+    //       toastr.error(data.message)
+    //     }
+    //   }
+    // })
   })
 
   function cekGunakanPoint(){
